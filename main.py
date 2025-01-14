8b339ede4537ce2d64fa09c5664c9911384c62a0
Gerardo Huerta <el_gera@Gerardos-MacBook-Pro.local>
2025-01-14 18:46:20 +0100

    Add comments

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import turtle
-import pandas
 import os
+import pandas
 import string
+import turtle
 
 """Check if the file exists and remove it"""
 if os.path.exists("states_to_learn.csv"):
     os.remove("states_to_learn.csv")
 
+"""Set up the screen"""
 image = "./mx.gif"
 screen = turtle.Screen()
 screen.addshape(image)
@@ -15,9 +16,11 @@ screen.setup(width=1000, height=750)
 turtle.shape(image)
 guessed_states: list[str] = []
 
+"""Read the data from CSV file"""
 data = pandas.read_csv("./32_states.csv")
 all_states = data.state.to_list()
 
+"""Loop to guess the states"""
 while len(guessed_states) < 32:
     if len(guessed_states) == 0:
         title = "Guess the State"
@@ -27,11 +30,14 @@ while len(guessed_states) < 32:
         prompt = "What's another state's name?"
 
     answer_state = screen.textinput(title, prompt)
+    """Check if the answer is correct"""
     if answer_state:
         capitalized_answer = string.capwords(answer_state)
+        """Exit the game"""
         if capitalized_answer == "Exit":
             break
         answer = data[data.state == capitalized_answer]
+        """Validate the answer's not empty and not already guessed"""
         if not answer.empty and capitalized_answer not in guessed_states:
             t = turtle.Turtle()
             t.hideturtle()
@@ -40,7 +46,7 @@ while len(guessed_states) < 32:
             t.write(capitalized_answer)
             guessed_states.append(capitalized_answer)
 
-# Write down a new file with the missing states
+"""States to learn. Write down the states that were not guessed"""
 states_to_learn: list[str] = []
 for state in all_states:
     if state not in guessed_states:
